f1bbd1f6eed59062a0943e04251ec64e41731d3c
Umang Tiwari <utiwari18@gmail.com>
2022-05-26 15:40:03 +0530

    Time: 43 ms (75.15%), Space: 19 MB (85.38%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,54 +1,34 @@
+/*
+
+
+Solution Reference:
+https://leetcode.com/problems/merge-intervals/discuss/1644017/C%2B%2B-EASY-TO-SOLVE-oror-Beginner-Friendly-with-Detailed-Explanations
+
+Check if the coming interval lies within the previous interval or extends the previous interval
+and push inside the resultant vector
+
+*/
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         
         sort(intervals.begin(), intervals.end());
         
-        stack<pair<int, int>> st;
-        
-        for(int i=0; i<intervals.size(); i++){
-            
-            if(!st.empty()){
-                int newX, newY;
-                
-                int x1 = st.top().first;
-                int y1 = st.top().second;
-                
-                int x2 = intervals[i][0];
-                int y2 = intervals[i][1];
-            
-                // This means the all numbers of second interval come inside the first interval
-                if(x2 <= y1 && y2 <= y1){
-                    st.pop();
-                    st.push({x1, y1});
-                    continue;
-                }
-                
-                if(x2 <= y1 && y2 >= y1){
-                    st.pop();
-                    st.push({x1, y2});
-                    continue;
-                }
-            }
-            
-            st.push({intervals[i][0], intervals[i][1]});
-            
-            
-            
-        }
-        
         vector<vector<int>> res;
         
-        while(!st.empty()){
-            vector<int> arr(2);
-            arr[0] = st.top().first;
-            arr[1] = st.top().second;
-            res.push_back(arr);
-            st.pop();
-            
-        }
+        res.push_back(intervals[0]);
         
-        reverse(res.begin(), res.end());
+        int j = 0;
+        
+        for(int i=1; i<intervals.size(); i++){
+            
+            if(res[j][1] >= intervals[i][0]) res[j][1] = max(res[j][1], intervals[i][1]);
+            else{
+                j++;
+                res.push_back(intervals[i]);
+            }
+        }
         
         return res;
         
