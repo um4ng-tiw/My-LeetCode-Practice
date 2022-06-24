7fefb30cb936013eef8402212b8ec3d54d1d1479
Umang Tiwari <utiwari18@gmail.com>
2022-06-24 21:56:34 +0530

    Time: 51 ms (78.32%), Space: 30 MB (51.05%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        priority_queue<int> pq;
+        long maxSum = 0;
+        
+        for(auto it: target){
+            pq.push(it);
+            maxSum+=it;
+        }
+        
+        while(!pq.empty()){
+            int maxEl = pq.top();
+            pq.pop();
+            long remSum = maxSum - maxEl;
+            
+            if(remSum == 1 || maxEl == 1) return true;
+            if(remSum == 0 || maxEl < remSum) return false;
+            int difference = maxEl % remSum;
+            
+            if(difference == 0) return false;
+            pq.push(difference);
+            
+            maxSum = difference + remSum; 
+        }
+        
+        return true;
+    }
+};
