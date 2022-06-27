9dd6fb63f02c92ad9898d1b3078223608844191e
Umang Tiwari <utiwari18@gmail.com>
2022-06-27 20:44:35 +0530

    Time: 56 ms (23.66%), Space: 13.7 MB (17.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -4,6 +4,7 @@ public:
         
         int maxEl = INT_MIN;
         for(int i=0; i<n.size(); i++){
+            if(n[i] == '9') return 9;
             maxEl = max(maxEl, n[i] - '0');
         }
         
