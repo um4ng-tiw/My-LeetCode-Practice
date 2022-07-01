3af93e00b1d5ba21519f713cc19074de796512ba
Umang Tiwari <utiwari18@gmail.com>
2022-07-01 21:23:18 +0530

    Time: 86 ms (43.10%), Space: 16.9 MB (43.71%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     static bool cmp(vector<int> &el1, vector<int>&el2){
-        if(el1[1] == el2[1]) return el1[0] > el2[0];
         return el1[1] > el2[1];
     }
     
@@ -13,14 +12,10 @@ public:
         
         
         for(auto it: boxTypes){
-           if(it[0] <= truckSize){
-               maxUnits += (it[0] * it[1]);
-               truckSize -= it[0];
-           }else if(truckSize == 0) break;
-            else{
-                maxUnits+= truckSize * it[1];
-                truckSize = 0;
-            }
+           int nUnits = min(it[0], truckSize);
+           maxUnits += nUnits * it[1];
+           truckSize -= nUnits;
+           if(!truckSize) break;
         }
         
         return maxUnits;
